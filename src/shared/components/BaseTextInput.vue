972ea68ed20ba6b1563c04509e0553373dd90429
Yevhenii Kravchenko <yevhenii.kravchenko@gmail.com>
2017-11-24 14:08:30 +0200

    Setup base components BaseTextInput, FormField

---
+++ src/shared/components/BaseTextInput.vue
@@ -0,0 +1,15 @@
+<template>
+  <input type="text" :value="value" @input="$emit('input', $event.target.value)" />
+</template>
+
+<script>
+export default {
+  name: 'BaseTextInput',
+  props: {
+    value: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
