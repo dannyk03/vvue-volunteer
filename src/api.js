350cb990815064b4891c23e861288ab32e7def34
Yevhenii Kravchenko <yevhenii.kravchenko@gmail.com>
2017-11-24 14:49:20 +0200

    Add todo

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ import { get, post } from '@/shared/utils/http';
 
 export const registerUser = data => post('api/users', data);
 export const login = data => post('/oauth/token', Object.assign({}, data, {
+  // TODO: store clientId & clientSecret in different place
   grantType: 'password',
   clientId: '1',
   clientSecret: 'uJrcxY2UspB3iajG3wrLjUEpurZM5k2538PMgDcz',
