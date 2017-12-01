61a5a0e208dcbc6bf0433eacb2dff662009dec2a
Yevhenii Kravchenko <yevhenii.kravchenko@gmail.com>
2017-12-01 15:24:45 +0200

    Create simple wrapper around console. Add features later if needed

---
+++ src/shared/utils/logger.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+export const log = text => console.log(text);
+
+export const info = text => console.info(text);
+
+export const warn = text => console.warn(text);
+
+export const error = text => console.error(text);
