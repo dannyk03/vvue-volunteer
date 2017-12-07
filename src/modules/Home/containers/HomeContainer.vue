04297d1edc1cfeaef229935ef7cc95b1be0b76af
Yevhenii Kravchenko <yevhenii.kravchenko@gmail.com>
2017-12-07 16:08:16 +0200

    Add form for submitting new users

--- src/modules/Home/containers/HomeContainer.vue
+++ src/modules/Home/containers/HomeContainer.vue
@@ -2,8 +2,22 @@
   <div>
     <h3>HomeContainer</h3>
 
-    <button @click="add">Add user</button>
-    <button @click="fetchUsers">Fetch users</button>
+    <h2>Add User:</h2>
+    <form @submit.prevent="addNewUser">
+      <vv-form-field label="Name">
+        <vv-base-text-input slot="control" v-model="newUser.name" />
+      </vv-form-field>
+
+      <vv-form-field label="Age">
+        <vv-base-text-input slot="control" v-model="newUser.age" />
+      </vv-form-field>
+
+      <button type="submit">Save User</button>
+    </form>
+
+
+    <!-- <button @click="add">Add user</button> -->
+    <button style="margin-top: 20px" @click="fetchUsers">Fetch users</button>
     <h2>Users:</h2>
     <div v-if="loading">Loading...</div>
     <div v-for="user in users" :key="user.id" @click="fetchOne(user.id)">
@@ -28,37 +42,47 @@
 
 <script>
 import { mapActions, mapGetters, mapMutations } from 'vuex'; // eslint-disable-line
+import VvFormField from '@/shared/components/FormField';
+import VvBaseTextInput from '@/shared/components/BaseTextInput';
 import VvHomeComponent from '../components/HomeComponent';
-import { users } from '../mutationTypes';
+
+import { users } from '../store';
 
 export default {
   name: 'HomeContainer',
   components: {
     VvHomeComponent,
+    VvFormField,
+    VvBaseTextInput,
   },
+  data: () => ({
+    newUser: {
+      name: '',
+      age: '',
+    },
+  }),
   computed: {
     ...mapGetters('home/users', {
-      users: users.getters.all,
-      loading: users.getters.allLoading,
+      users: users.getters.list,
+      loading: users.getters.listLoading,
       currentUser: users.getters.one,
       currentUserLoading: users.getters.oneLoading,
     }),
   },
   methods: {
     ...mapActions('home/users', {
-      fetchUsers: users.actions.fetch,
-      fetchOrders: users.actions.fetch,
+      fetchUsers: users.actions.fetchList,
       fetchOne: users.actions.fetchOne,
+      addUser: users.actions.post,
       remove: users.actions.delete,
     }),
-    ...mapMutations('home/users', {
-      addUser: users.mutations.add,
-    }),
-    add() {
-      this.addUser({
-        name: 'Paul Viskovich',
-        age: 41,
-      });
+    // ...mapMutations('home/users', {
+    //   addUser: users.mutations.add,
+    // }),
+    addNewUser() {
+      this.addUser({ ...this.newUser });
+      this.newUser.name = '';
+      this.newUser.age = '';
     },
   },
 };
