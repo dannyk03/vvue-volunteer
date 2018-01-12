f67d691dce0198b56ad3bf55353475973d826e70
Yevhenii Kravchenko <yevhenii.kravchenko@gmail.com>
2018-01-12 12:32:27 +0200

    Add base icon and logo

---
+++ src/shared/components/BaseIcon.vue
@@ -0,0 +1,39 @@
+<template>
+  <svgicon :name="getIconName()" :width="width" :height="height" :color="getColors()"></svgicon>
+</template>
+
+<script>
+  export default {
+    name: 'BaseIcon',
+    props: {
+      name: {
+        type: String,
+        required: true,
+      },
+      width: {
+        type: String,
+        default: '31',
+      },
+      height: {
+        type: String,
+        default: '31',
+      },
+      color: {
+        type: String,
+        default: 'black',
+      },
+      backgroundColor: {
+        type: String,
+        default: 'transparent',
+      },
+    },
+    methods: {
+      getIconName() {
+        return `${this.name}`;
+      },
+      getColors() {
+        return `${this.backgroundColor} ${this.color}`;
+      },
+    },
+  };
+</script>
---
+++ src/shared/components/Logo.vue
@@ -0,0 +1,24 @@
+<template>
+  <img :src="getPath(color)" alt="logo" />
+</template>
+
+<script>
+export default {
+  name: 'Logo',
+  props: {
+    color: String, // [white|grey]
+  },
+  methods: {
+    getPath(color) {
+      return `/static/images/logo-${color}@2x.png`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  img {
+    width: 131px;
+    height: 50px;
+  }
+</style>
