82e36d39083b15cea8441cc131ce73a43e6b333f
Yevhenii Kravchenko <yevhenii.kravchenko@gmail.com>
2018-01-19 17:13:50 +0200

    Avatar: FilePicker and WebcamPicker.

--- src/modules/Public/components/PersonalAvatar.vue
+++ src/modules/Public/components/PersonalAvatar.vue
@@ -1,34 +1,43 @@
 <template>
   <div class="personal-avatar-form">
 
-    <div class="avatar-wrapper">
-      <div class="avatar mt-4">
-         <vv-icon name="avatar" width="42" height="58" :fill="false" backgroundColor="#c1c1c1"  />
-         <div class="text mt-3">Awesome avatar here</div>
-      </div>
+    <div class="mt-4">
+      <vv-webcam-picker class="avatar-wrapper" @pick="showAvatarFromWebcam" v-if="takeFromWebcam" />
 
-      <div class="actions mt-4 mb-4">
-        <vv-base-button
-          class="mr-3"
-          @click.native="submitStep"
-          color="accent"
-          outline
-          >
-            Upload
-          </vv-base-button>
+      <div class="avatar-wrapper" v-else>
+        <div class="avatar">
+          <img v-if="avatarPreview" :src="avatarPreview" />
+          <div v-else>
+            <vv-icon name="avatar" width="42" height="58" :fill="false" backgroundColor="#c1c1c1"  />
+            <div class="text mt-3">Awesome avatar here</div>
+          </div>
+        </div>
+
+        <div class="actions mt-4 mb-4">
+          <vv-file-picker @pick="showAvatar">
+            <vv-base-button
+              class="mr-3"
+              color="accent"
+              outline
+            >
+              Upload
+            </vv-base-button>
+          </vv-file-picker>
 
           <vv-base-button
-            @click.native="submitStep"
+            @click.native="takeFromWebcam = true"
             color="accent"
             outline
-            >
-              Take from webcam
-            </vv-base-button>
+          >
+            Take from webcam
+          </vv-base-button>
+        </div>
+
       </div>
     </div>
 
-
     <vv-base-button
+      v-if="!takeFromWebcam"
       class="mt-5"
       @click.native="submitStep"
       color="accent"
@@ -43,6 +52,8 @@ import { mapMutations } from 'vuex';
 import VvBaseTextInput from '@/shared/components/BaseTextInput';
 import VvBaseButton from '@/shared/components/BaseButton';
 import VvIcon from '@/shared/components/BaseIcon';
+import VvFilePicker from '@/shared/components/FilePicker';
+import VvWebcamPicker from '@/shared/components/WebcamPicker';
 import '@/../static/icons/compiled-svg/avatar';
 
 import { UPDATE_ONBOARDING_INFO } from '../mutationTypes';
@@ -53,6 +64,8 @@ export default {
     VvBaseTextInput,
     VvBaseButton,
     VvIcon,
+    VvFilePicker,
+    VvWebcamPicker,
   },
   data: () => ({
     model: {
@@ -60,6 +73,8 @@ export default {
       position: '',
       education: '',
     },
+    avatarPreview: null,
+    takeFromWebcam: false,
   }),
   methods: {
     submitStep() {
@@ -73,6 +88,13 @@ export default {
     ...mapMutations('public', {
       setNameInfo: UPDATE_ONBOARDING_INFO,
     }),
+    showAvatar(file) {
+      this.avatarPreview = window.URL.createObjectURL(file);
+    },
+    showAvatarFromWebcam(base64) {
+      this.takeFromWebcam = false;
+      this.avatarPreview = base64;
+    },
   },
   mounted() {
     const data = this.$store.getters['public/getPersonalInfo'];
@@ -92,11 +114,19 @@ export default {
       flex-direction: column;
       align-items: center;
 
+      .actions {
+        display: flex;
+      }
+
       .avatar {
         width: 190px;
         height: 190px;
         border: 1px dashed $secondaryGrey;
         flex-direction: column;
+
+        img {
+          object-fit: cover;
+        }
 
         .text {
           width: 120px;
